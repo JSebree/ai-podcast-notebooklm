6e527932ddd25c2f428b6bf3c7e0815389eaf43e
JSebree <30639265+JSebree@users.noreply.github.com>
2025-04-27 12:14:10 -0400

    Update curator_agent.py

--- agents/curator_agent.py
+++ agents/curator_agent.py
@@ -14,7 +14,11 @@ def get_agent():
         role="News Curator",
         goal=(
             "Identify the five most important breaking stories in AI, "
-            "quantum computing, and robotics every 24 hours."
+            "quantum computing, and robotics every from the past 36 hours."
+            "**IMPORTANT! only gather stpries from the past 36 hours from this date."
+            "Pull stories from reputale publications, Linkedin and Twitter posts "
+            "from industry leaders and influencers, and YouTube influencer who specialize "
+            "in AI, quantum computing, and robotics." 
         ),
         backstory=(
             "You are an experienced tech journalist with a keen eye for "
